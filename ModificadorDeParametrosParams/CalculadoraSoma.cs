47d5827ee3488280380587244ac9698d4de39367
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-10 18:53:55 -0300

    Calculadora de soma com aplicacao do parametro Params

---
+++ ModificadorDeParametrosParams/CalculadoraSoma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using System.Text;
+
+namespace ModificadorDeParametrosParams
+{
+    class CalculadoraSoma
+    {
+        // Metodo:
+        public static int Sum(params int[] numero)
+        {
+            int sum = 0;
+            for (int i = 0; i < numero.Length; i++)
+            {
+                sum += numero[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ ModificadorDeParametrosParams/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ModificadorDeParametrosParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciando calculadora de soma:
+
+            int s0 = CalculadoraSoma.Sum(5);
+            int s1 = CalculadoraSoma.Sum(2, 3);
+            int s2 = CalculadoraSoma.Sum(2, 4, 3);
+
+            Console.WriteLine(s0);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+        }
+    }
+}
